fa53b1c6a5f9cc9aa70888bebab7250129339757
vanling1111 <vanling1111@gmail.com>
2025-12-01 09:32:33 +0800

    Clean up: remove unnecessary hooks (use @vueuse/core and reka-ui instead)

---
+++ packages/core/src/hooks/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Glacial UI Hooks
+ * 
+ * 注意：大部分通用 hooks 来自第三方库：
+ * - @vueuse/core: useVModel, reactiveOmit, useEventListener 等
+ * - reka-ui: useForwardPropsEmits, useForwardProps 等
+ * 
+ * 这里只导出 Glacial UI 特有的 hooks
+ */
+
+// 如果需要添加 Glacial UI 特有的 hooks，在这里导出
+// export { useGlacialTheme } from './useGlacialTheme'
